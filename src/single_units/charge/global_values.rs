cbf3423df09184b3f775da62c48dc73c6af5efae
hass <hassoune.liban77@outlook.com>
2022-06-20 19:50:15 +0200

    Fix public elements
    Signed-off-by: hass <hassoune.liban77@outlook.com>

--- src/single_units/charge/global_values.rs
+++ src/single_units/charge/global_values.rs
@@ -1,12 +1,5 @@
 pub mod charge {
-<<<<<<< HEAD
     pub const DEFAULT_DICE: isize = 6;
     pub const DEFAULT_ROLL: isize = 1;
     pub const ARMOUR_SAVE_THRESHOLD: isize = 7;
-=======
-
-    const DEFAULT_DICE: usize = 6;
-    const DEFAULT_ROLL: usize = 1;
-    const ARMOUR_SAVE_THRESHOLD: usize = 7;
->>>>>>> d1a7a7726926d528e43110a57a07199805cd1996
 }
