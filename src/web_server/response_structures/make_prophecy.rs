74f159ccead03ba208cc1d060f95631bfc49471c
Jordan-B1 <bankolejordan75@gmail.com>
2023-01-12 01:54:44 +0100

    test(web_server): add some tests to the web_server

--- src/web_server/response_structures/make_prophecy.rs
+++ src/web_server/response_structures/make_prophecy.rs
@@ -2,7 +2,7 @@
 //!
 //! This module contains all the data structures needed by the make_prophecy response
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Clone)]
 pub struct RegimentData {
     pub nb_rows: usize,
     pub nb_cols: usize,
@@ -10,14 +10,14 @@ pub struct RegimentData {
     pub points: usize,
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Clone)]
 pub struct Prediction {
     pub attacking_regiment: RegimentData,
     pub defending_regiment: RegimentData,
     pub occurrence_probability: f64,
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, PartialEq, Clone)]
 pub struct MakeProphecyResponse {
     best_case: Prediction,
     worst_case: Prediction,
@@ -104,4 +104,83 @@ impl MakeProphecyResponse {
             average_case.unwrap(),
         )
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_regiment_data_new() {
+        let regiment_data: RegimentData = RegimentData::new(1, 2, 3, 4);
+        assert_eq!(regiment_data.nb_rows, 1);
+        assert_eq!(regiment_data.nb_cols, 2);
+        assert_eq!(regiment_data.nb_models, 3);
+        assert_eq!(regiment_data.points, 4);
+    }
+
+    #[test]
+    fn test_prediction_new() {
+        let prediction = Prediction::new(
+            RegimentData::new(1, 2, 3, 4),
+            RegimentData::new(5, 6, 7, 8),
+            0.5,
+        );
+        assert_eq!(prediction.attacking_regiment.nb_rows, 1);
+        assert_eq!(prediction.attacking_regiment.nb_cols, 2);
+        assert_eq!(prediction.attacking_regiment.nb_models, 3);
+        assert_eq!(prediction.attacking_regiment.points, 4);
+        assert_eq!(prediction.defending_regiment.nb_rows, 5);
+        assert_eq!(prediction.defending_regiment.nb_cols, 6);
+        assert_eq!(prediction.defending_regiment.nb_models, 7);
+        assert_eq!(prediction.defending_regiment.points, 8);
+        assert_eq!(prediction.occurrence_probability, 0.5);
+    }
+
+    #[test]
+    fn test_make_prophecy_response_new() {
+        let best_prediction: Prediction = Prediction::new(
+            RegimentData::new(1, 2, 3, 4),
+            RegimentData::new(5, 6, 7, 8),
+            0.5,
+        );
+        let worst_prediction: Prediction = Prediction::new(
+            RegimentData::new(9, 10, 11, 12),
+            RegimentData::new(13, 14, 15, 16),
+            0.6,
+        );
+        let average_prediction: Prediction = Prediction::new(
+            RegimentData::new(17, 18, 19, 20),
+            RegimentData::new(21, 22, 23, 24),
+            0.7,
+        );
+        let copies: (Prediction, Prediction, Prediction) = (
+            best_prediction.clone(),
+            worst_prediction.clone(),
+            average_prediction.clone(),
+        );
+        let make_prophecy_response =
+            MakeProphecyResponse::new(best_prediction, worst_prediction, average_prediction);
+        assert_eq!(make_prophecy_response.best_case == copies.0, true);
+        assert_eq!(make_prophecy_response.worst_case == copies.1, true);
+        assert_eq!(make_prophecy_response.average_case == copies.2, true);
+    }
+
+    /*#[test]
+    fn test_make_prophecy_response_from_dict() {
+        let prophecies: std::collections::HashMap<
+            crate::fight::ComputeCase,
+            crate::prediction::Prediction,
+        > = std::collections::HashMap::new();
+        prophecies.insert(
+            crate::fight::ComputeCase::BEST,
+            crate::prediction::Prediction::new(
+                crate::regiment::Regiment::new(1, 2, 3, 4, None),
+                crate::regiment::Regiment::new(5, 6, 7, 8, None),
+                0.5,
+            ),
+        );
+
+        let make_prophecy_response = MakeProphecyResponse::from_dict(prophecies);
+    }*/
 }
