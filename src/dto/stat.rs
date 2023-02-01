b7ea6749825518fa89618ff2c6555efa4cb4ff10
Pepiloto <pepiloto@pepiloto.dev>
2023-01-31 23:53:45 -0500

    test: Add tests for the dto and modify the python script for updates in the rust code

--- src/dto/stat.rs
+++ src/dto/stat.rs
@@ -161,3 +161,94 @@ impl StatsDto {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_stats_dto() {
+        let stats = StatsDto::new(
+            GlobalStatsDto::new(1, 2, 3),
+            DefensiveStatsDto::new(4, 5, 6, 7, 8),
+            OffensiveStatsDto::new(9, 10, 11, 12, 13),
+        );
+
+        assert_eq!(stats.advance, 1);
+        assert_eq!(stats.march, 2);
+        assert_eq!(stats.discipline, 3);
+        assert_eq!(stats.health_points, 4);
+        assert_eq!(stats.defense, 5);
+        assert_eq!(stats.resilience, 6);
+        assert_eq!(stats.armour, 7);
+        assert_eq!(stats.aegis, 8);
+        assert_eq!(stats.attack, 9);
+        assert_eq!(stats.offensive, 10);
+        assert_eq!(stats.strength, 11);
+        assert_eq!(stats.armour_penetration, 12);
+        assert_eq!(stats.agility, 13);
+    }
+
+    #[test]
+    fn test_stats_dto_hydrate() {
+        let stats = StatsDto::new(
+            GlobalStatsDto::new(1, 2, 3),
+            DefensiveStatsDto::new(4, 5, 6, 7, 8),
+            OffensiveStatsDto::new(9, 10, 11, 12, 13),
+        )
+        .hydrate();
+
+        assert_eq!(stats.get_advance(), 1);
+        assert_eq!(stats.get_march(), 2);
+        assert_eq!(stats.get_discipline(), 3);
+        assert_eq!(stats.get_health_points(), 4);
+        assert_eq!(stats.get_defense(), 5);
+        assert_eq!(stats.get_resilience(), 6);
+        assert_eq!(stats.get_armour(), 7);
+        assert_eq!(stats.get_aegis(), 8);
+        assert_eq!(stats.get_attack(), 9);
+        assert_eq!(stats.get_offensive(), 10);
+        assert_eq!(stats.get_strength(), 11);
+        assert_eq!(stats.get_armour_penetration(), 12);
+        assert_eq!(stats.get_agility(), 13);
+    }
+
+    #[test]
+    fn test_stats_dto_dehydrate() {
+        let stats = StatsDto::dehydrate(&crate::stat::Stats::new(
+            crate::stat::GlobalStats {
+                advance: 1,
+                march: 2,
+                discipline: 3,
+            },
+            crate::stat::DefensiveStats {
+                health_points: 4,
+                defense: 5,
+                resilience: 6,
+                armour: 7,
+                aegis: 8,
+            },
+            crate::stat::OffensiveStats {
+                attack: 9,
+                offensive: 10,
+                strength: 11,
+                armour_penetration: 12,
+                agility: 13,
+            },
+        ));
+
+        assert_eq!(stats.advance, 1);
+        assert_eq!(stats.march, 2);
+        assert_eq!(stats.discipline, 3);
+        assert_eq!(stats.health_points, 4);
+        assert_eq!(stats.defense, 5);
+        assert_eq!(stats.resilience, 6);
+        assert_eq!(stats.armour, 7);
+        assert_eq!(stats.aegis, 8);
+        assert_eq!(stats.attack, 9);
+        assert_eq!(stats.offensive, 10);
+        assert_eq!(stats.strength, 11);
+        assert_eq!(stats.armour_penetration, 12);
+        assert_eq!(stats.agility, 13);
+    }
+}
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,262 +1,171 @@
-// use maths::fight;
-// use maths::prediction;
-// use maths::regiment;
-// mod initialize_units;
+use maths::fight;
+use maths::regiment;
+mod initialize_units;
 
-// #[test]
-// fn test_warriors_against_heavy_infantry() {
-//     let attacking_position: maths::web_server::converter::web_objects::AttackPosition = maths::web_server::converter::web_objects::AttackPosition::FRONT;
-//     let attacking: regiment::Regiment = initialize_units::initialize_warriors();
-//     let defending: regiment::Regiment = initialize_units::initialize_heavy_infantry();
-//     let res: std::collections::HashMap<fight::ComputeCase, prediction::Prediction> =
-//         fight::compute_turn(attacking_position, &attacking, &defending);
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         9
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         11
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         6
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         1
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         3
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         3
-//     );
-// }
+#[test]
+fn test_warriors_against_heavy_infantry() {
+    let attacking_position: fight::AttackPosition = maths::fight::AttackPosition::FRONT;
+    let attacking: regiment::Regiment = initialize_units::initialize_warriors();
+    let defending: regiment::Regiment = initialize_units::initialize_heavy_infantry();
+    let res: fight::FightPredictionResult =
+        fight::compute_turn(attacking_position, &attacking, &defending);
+    assert_eq!(
+        res.get_mean_case().get_attacking_regiment().get_points(),
+        9
+    );
+    assert_eq!(
+        res.get_mean_case().get_defending_regiment().get_points(),
+        1
+    );
+    assert_eq!(
+        res.get_best_case().get_attacking_regiment().get_points(),
+        11
+    );
+    assert_eq!(
+        res.get_best_case().get_defending_regiment().get_points(),
+        3
+    );
+    assert_eq!(
+        res.get_worst_case().get_attacking_regiment().get_points(),
+        6
+    );
+    assert_eq!(
+        res.get_worst_case().get_defending_regiment().get_points(),
+        3
+    );
+}
 
-// #[test]
-// fn test_warriors_against_wildhorn_herd() {
-//     let attacking_position: maths::web_server::converter::web_objects::AttackPosition = maths::web_server::converter::web_objects::AttackPosition::FRONT;
-//     let attacking: regiment::Regiment = initialize_units::initialize_warriors();
-//     let defending: regiment::Regiment = initialize_units::initialize_wildhorn_herd();
-//     let res: std::collections::HashMap<fight::ComputeCase, prediction::Prediction> =
-//         fight::compute_turn(attacking_position, &attacking, &defending);
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         6
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         9
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         6
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         1
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         3
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         3
-//     );
-// }
+#[test]
+fn test_warriors_against_wildhorn_herd() {
+    let attacking_position: fight::AttackPosition = maths::fight::AttackPosition::FRONT;
+    let attacking: regiment::Regiment = initialize_units::initialize_warriors();
+    let defending: regiment::Regiment = initialize_units::initialize_wildhorn_herd();
+    let res: fight::FightPredictionResult =
+        fight::compute_turn(attacking_position, &attacking, &defending);
+    assert_eq!(
+        res.get_mean_case().get_attacking_regiment().get_points(),
+        6
+    );
+    assert_eq!(
+        res.get_mean_case().get_defending_regiment().get_points(),
+        1
+    );
+    assert_eq!(
+        res.get_best_case().get_attacking_regiment().get_points(),
+        9
+    );
+    assert_eq!(
+        res.get_best_case().get_defending_regiment().get_points(),
+        3
+    );
+    assert_eq!(
+        res.get_worst_case().get_attacking_regiment().get_points(),
+        6
+    );
+    assert_eq!(
+        res.get_worst_case().get_defending_regiment().get_points(),
+        3
+    );
+}
 
-// #[test]
-// fn test_imps_against_wildhorn_herd() {
-//     let attacking_position: maths::web_server::converter::web_objects::AttackPosition = maths::web_server::converter::web_objects::AttackPosition::FRONT;
-//     let attacking: regiment::Regiment = initialize_units::initialize_imps();
-//     let defending: regiment::Regiment = initialize_units::initialize_wildhorn_herd();
-//     let res: std::collections::HashMap<fight::ComputeCase, prediction::Prediction> =
-//         fight::compute_turn(attacking_position, &attacking, &defending);
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         2
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         4
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         6
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         2
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         3
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         4
-//     );
-// }
+#[test]
+fn test_imps_against_wildhorn_herd() {
+    let attacking_position: fight::AttackPosition = maths::fight::AttackPosition::FRONT;
+    let attacking: regiment::Regiment = initialize_units::initialize_imps();
+    let defending: regiment::Regiment = initialize_units::initialize_wildhorn_herd();
+    let res: fight::FightPredictionResult =
+        fight::compute_turn(attacking_position, &attacking, &defending);
+    assert_eq!(
+        res.get_mean_case().get_attacking_regiment().get_points(),
+        2
+    );
+    assert_eq!(
+        res.get_mean_case().get_defending_regiment().get_points(),
+        2
+    );
+    assert_eq!(
+        res.get_best_case().get_attacking_regiment().get_points(),
+        4
+    );
+    assert_eq!(
+        res.get_best_case().get_defending_regiment().get_points(),
+        3
+    );
+    assert_eq!(
+        res.get_worst_case().get_attacking_regiment().get_points(),
+        6
+    );
+    assert_eq!(
+        res.get_worst_case().get_defending_regiment().get_points(),
+        4
+    );
+}
 
-// #[test]
-// fn test_imps_against_heavy_infantry() {
-//     let attacking_position: maths::web_server::converter::web_objects::AttackPosition = maths::web_server::converter::web_objects::AttackPosition::FRONT;
-//     let attacking: regiment::Regiment = initialize_units::initialize_imps();
-//     let defending: regiment::Regiment = initialize_units::initialize_heavy_infantry();
-//     let res: std::collections::HashMap<fight::ComputeCase, prediction::Prediction> =
-//         fight::compute_turn(attacking_position, &attacking, &defending);
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         2
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         4
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         4
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         2
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         3
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         4
-//     );
-// }
+#[test]
+fn test_imps_against_heavy_infantry() {
+    let attacking_position: fight::AttackPosition = maths::fight::AttackPosition::FRONT;
+    let attacking: regiment::Regiment = initialize_units::initialize_imps();
+    let defending: regiment::Regiment = initialize_units::initialize_heavy_infantry();
+    let res: fight::FightPredictionResult =
+        fight::compute_turn(attacking_position, &attacking, &defending);
+    assert_eq!(
+        res.get_mean_case().get_attacking_regiment().get_points(),
+        2
+    );
+    assert_eq!(
+        res.get_mean_case().get_defending_regiment().get_points(),
+        2
+    );
+    assert_eq!(
+        res.get_best_case().get_attacking_regiment().get_points(),
+        4
+    );
+    assert_eq!(
+        res.get_best_case().get_defending_regiment().get_points(),
+        3
+    );
+    assert_eq!(
+        res.get_worst_case().get_attacking_regiment().get_points(),
+        4
+    );
+    assert_eq!(
+        res.get_worst_case().get_defending_regiment().get_points(),
+        4
+    );
+}
 
-// #[test]
-// fn test_warriors_against_warriors() {
-//     let attacking_position: maths::web_server::converter::web_objects::AttackPosition = maths::web_server::converter::web_objects::AttackPosition::FRONT;
-//     let attacking: regiment::Regiment = initialize_units::initialize_warriors();
-//     let defending: regiment::Regiment = initialize_units::initialize_warriors();
-//     let res: std::collections::HashMap<fight::ComputeCase, prediction::Prediction> =
-//         fight::compute_turn(attacking_position, &attacking, &defending);
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         6
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         9
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_attacking_regiment()
-//             .get_points(),
-//         6
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::MEAN)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         5
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::BEST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         5
-//     );
-//     assert_eq!(
-//         res.get(&fight::ComputeCase::WORST)
-//             .unwrap()
-//             .get_defending_regiment()
-//             .get_points(),
-//         8
-//     );
-// }
+#[test]
+fn test_warriors_against_warriors() {
+    let attacking_position: fight::AttackPosition = maths::fight::AttackPosition::FRONT;
+    let attacking: regiment::Regiment = initialize_units::initialize_warriors();
+    let defending: regiment::Regiment = initialize_units::initialize_warriors();
+    let res: fight::FightPredictionResult =
+        fight::compute_turn(attacking_position, &attacking, &defending);
+    assert_eq!(
+        res.get_mean_case().get_attacking_regiment().get_points(),
+        6
+    );
+    assert_eq!(
+        res.get_mean_case().get_defending_regiment().get_points(),
+        5
+    );
+    assert_eq!(
+        res.get_best_case().get_attacking_regiment().get_points(),
+        9
+    );
+    assert_eq!(
+        res.get_best_case().get_defending_regiment().get_points(),
+        5
+    );
+    assert_eq!(
+        res.get_worst_case().get_attacking_regiment().get_points(),
+        6
+    );
+    assert_eq!(
+        res.get_worst_case().get_defending_regiment().get_points(),
+        8
+    );
+}
 
 // #[test]
 // fn test_imps_against_imps() {
