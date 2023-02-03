74c045fa815ab56cca99fb849005fd4fb0e6c20d
Pepiloto <pepiloto@pepiloto.dev>
2023-02-02 19:19:09 -0500

    test: Add missing tests for Dto module

--- src/dto/model.rs
+++ src/dto/model.rs
@@ -81,4 +81,86 @@ impl ModelDto {
             banner_bearer: model.is_banner_bearer(),
         }
     }
+
+    /// Get the stats of the model
+    ///
+    /// # Return
+    ///
+    /// StatsDto: The stats of the model
+    pub fn get_stats(&self) -> &super::stat::StatsDto {
+        &self.stats
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_model_dto_hydrate() {
+        let model_dto = super::ModelDto::new(
+            crate::dto::stat::StatsDto::new(
+                crate::dto::stat::GlobalStatsDto::new(1, 2, 3),
+                crate::dto::stat::DefensiveStatsDto::new(4, 5, 6, 7, 8),
+                crate::dto::stat::OffensiveStatsDto::new(9, 10, 11, 12, 13),
+            ),
+            vec![],
+            true,
+        );
+        let model: crate::model::Model = model_dto.hydrate();
+        assert_eq!(model.get_stats().get_advance(), 1);
+        assert_eq!(model.get_stats().get_march(), 2);
+        assert_eq!(model.get_stats().get_discipline(), 3);
+        assert_eq!(model.get_stats().get_health_points(), 4);
+        assert_eq!(model.get_stats().get_defense(), 5);
+        assert_eq!(model.get_stats().get_resilience(), 6);
+        assert_eq!(model.get_stats().get_armour(), 7);
+        assert_eq!(model.get_stats().get_aegis(), 8);
+        assert_eq!(model.get_stats().get_attack(), 9);
+        assert_eq!(model.get_stats().get_offensive(), 10);
+        assert_eq!(model.get_stats().get_strength(), 11);
+        assert_eq!(model.get_stats().get_armour_penetration(), 12);
+        assert_eq!(model.get_stats().get_agility(), 13);
+    }
+
+    #[test]
+    fn test_model_dto_dehydrate() {
+        let model = crate::model::Model::new(
+            crate::stat::Stats::new(
+                crate::stat::GlobalStats {
+                    advance: 1,
+                    march: 2,
+                    discipline: 3,
+                },
+                crate::stat::DefensiveStats {
+                    health_points: 4,
+                    defense: 5,
+                    resilience: 6,
+                    armour: 7,
+                    aegis: 8,
+                },
+                crate::stat::OffensiveStats {
+                    attack: 9,
+                    offensive: 10,
+                    strength: 11,
+                    armour_penetration: 12,
+                    agility: 13,
+                },
+            ),
+            vec![],
+            true,
+        );
+        let model_dto: super::ModelDto = super::ModelDto::dehydrate(&model);
+        assert_eq!(model_dto.get_stats().get_advance(), 1);
+        assert_eq!(model_dto.get_stats().get_march(), 2);
+        assert_eq!(model_dto.get_stats().get_discipline(), 3);
+        assert_eq!(model_dto.get_stats().get_health_points(), 4);
+        assert_eq!(model_dto.get_stats().get_defense(), 5);
+        assert_eq!(model_dto.get_stats().get_resilience(), 6);
+        assert_eq!(model_dto.get_stats().get_armour(), 7);
+        assert_eq!(model_dto.get_stats().get_aegis(), 8);
+        assert_eq!(model_dto.get_stats().get_attack(), 9);
+        assert_eq!(model_dto.get_stats().get_offensive(), 10);
+        assert_eq!(model_dto.get_stats().get_strength(), 11);
+        assert_eq!(model_dto.get_stats().get_armour_penetration(), 12);
+        assert_eq!(model_dto.get_stats().get_agility(), 13);
+    }
 }
