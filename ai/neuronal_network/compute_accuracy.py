77496aeb044950dece868f29dd0f3735a95c7378
Jordan-B1 <bankolejordan75@gmail.com>
2023-07-04 23:13:52 +0200

    docs(clean_ai_repository): add some functions documentation

--- ai/neuronal_network/compute_accuracy.py
+++ ai/neuronal_network/compute_accuracy.py
@@ -12,10 +12,24 @@ test_size = 1000
 
 
 def compute_points_distance(p1, p2):
+    """This function compute the distance between two points
+
+    Args:
+        p1 (list(int)): First point
+        p2 (list(int)): Second point
+
+    Returns:
+        float: The distance between the two points
+    """
     return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 
 def build_test_dataset():
+    """This function build a test dataset from the trainning data
+
+    Returns:
+        list(match): A list of matchs according to the format of the trainning data
+    """
     test_dataset = []
     for i in range(test_size):
         match = format_json_match(random.choice(data))
@@ -26,6 +40,15 @@ def build_test_dataset():
 
 
 def compute_predictions(model, units):
+    """This function compute the predictions of the model for a list of units
+
+    Args:
+        model (keras.model): The model to use
+        units (list(unit)): The list of units to predict
+
+    Returns:
+        list(list(int)): The list of predictions
+    """
     points = []
     for x in units:
         prediction = model.predict(np.array([x]))
@@ -34,15 +57,25 @@ def compute_predictions(model, units):
 
 
 def compute_distance(points, scores):
+    """Build a list of distances between the predictions and the scores
+
+    Args:
+        points (list(list(int))): The list of predictions
+        scores (list(list(int))): The list of scores
+
+    Returns:
+        list(int): The list of distances between the predictions and the actual scores
+    """
     distances = []
     for i in range(len(points)):
         distances.append(compute_points_distance(points[i], scores[i]))
     return distances
 
 
-test_dataset = build_test_dataset()
-(units, scores, _) = format_matchs(test_dataset)
+if __name__ == '__main__':
+    test_dataset = build_test_dataset()
+    (units, scores, _) = format_matchs(test_dataset)
 
-predictions = compute_predictions(model, units)
-distances = compute_distance(predictions, scores)
-print(sum(distances) / len(distances))
+    predictions = compute_predictions(model, units)
+    distances = compute_distance(predictions, scores)
+    print(sum(distances) / len(distances))
